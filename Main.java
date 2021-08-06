a1ada4274e7d490a80827d7ae3a3a9c387760f05
Jarvis0420 <sagar.shinde98765@gmail.com>
2021-08-06 23:53:15 +0530

    FinalChanges

--- Main.java
+++ Main.java
@@ -17,18 +17,11 @@ public class Main {
 		
 		char live = '1', dead = '0';
 		
-		int rows, cols;
-		GameOfCells obj;
-		char[][] nextGen;
+		int rows=0, cols=0;
 		
 		switch(choice) {
 		case 1:
-			obj = new GameOfCells('1', '0');
-			nextGen = obj.getNextGenerationBoard(board);
-			
-			System.out.println("Input Board:");
-			obj.printBoard(board);
-			System.out.print("Live is represented as "+live+"\nDead is represented as "+dead);
+			userInputBoard(false, board, rows, cols, live, dead);
 			break;
 			
 		case 2:
@@ -47,66 +40,24 @@ public class Main {
 				System.out.print("Maximum number of cells is 100\nPlease try again");
 				throw new IllegalArgumentException("Maximum number of cells is 100");
 			}
-			
-			char[][] board1 = userInputBoard(rows, cols);
-			obj = new GameOfCells(live, dead);
-			nextGen = obj.getNextGenerationBoard(board1);
-			
-			System.out.println("Input Board:");
-			obj.printBoard(board1);
-			System.out.print("Live is represented as "+live+"\nDead is represented as "+dead);
+			userInputBoard(true,board, rows, cols, live, dead);
 			break;
 		default:
 			System.out.print("Invalid choice entered");
 			throw new IllegalArgumentException("Invalid input");
 		}
-		System.out.println("\n\n");
-		
-		int gen = 1;
-		boolean flag = true;
-		
-		while(flag) {
-			System.out.println("Generation "+gen);
-			obj.printBoard(nextGen);
-			
-			System.out.print("Press \n1 to continue\n2 to exit\n3 to search element\nchoice = ");
-			if(sc.hasNextInt()) {
-				choice = sc.nextInt();
-			}
-			else
-				choice = 2;
-			
-			switch(choice) {
-			case 1:
-				break;
-			case 2:
-				flag = false;
-				break;
-			case 3:
-				System.out.print("Enter cell name in row_column format: ");
-				String name = sc.next();
-				System.out.println("State of cell "+name+": "+obj.getCellState(nextGen, name));
-				break;
-			default:
-				System.out.print("Invalid choice entered");
-				flag = false;
-			}
-			
-			if(flag) {
-				gen++;
-				nextGen = obj.getNextGenerationBoard(nextGen);
-			}
-			System.out.println("\n\n");
-		}
-		
 		sc.close();
-
+		
 	}
 	
-	static char[][] userInputBoard(int rows, int cols) {
+	
+	
+	static void userInputBoard(boolean inputFlag, char[][] inputBoard, int rows, int cols, char live, char dead) {
+		
 		try (Scanner sc1 = new Scanner(System.in)) {
+			if(inputFlag) {
 			System.out.println("Enter each row in space separated format:");
-			char[][] inputBoard = new char[rows][];
+			inputBoard = new char[rows][];
 			for(int i = 0; i < rows; i++) {
 				String temp = sc1.nextLine();
 				String[] currRow = temp.split(" ");
@@ -122,8 +73,53 @@ public class Main {
 					inputBoard[i] = row;
 				}
 			}
-			sc1.close();
-			return inputBoard;
+			
+			}
+			
+			GameOfCells obj = new GameOfCells(live, dead);
+			char[][] nextGen = obj.getNextGenerationBoard(inputBoard);
+			
+			System.out.println("Input Board:");
+			obj.printBoard(inputBoard);
+			System.out.println("Live is represented as "+live+"\nDead is represented as "+dead+"\n\n");
+			
+			int gen = 1;
+			boolean flag = true;
+			
+				while(flag) {
+					System.out.println("Generation "+gen);
+					obj.printBoard(nextGen);
+					int choice = 0;
+					
+					System.out.print("Press \n1 to continue\n2 to exit\n3 to search element\nchoice = ");
+					if(sc1.hasNextInt()) {
+						choice = sc1.nextInt();
+					}
+					else
+						choice = 5;
+					
+					switch(choice) {
+					case 1:
+						break;
+					case 2:
+						flag = false;
+						break;
+					case 3:
+						System.out.print("Enter cell name in row_column format: ");
+						String name = sc1.next();
+						System.out.println("State of cell "+name+": "+obj.getCellState(nextGen, name));
+						break;
+					default:
+						System.out.print("Invalid choice entered");
+						flag = false;
+					}
+					
+					if(flag) {
+						gen++;
+						nextGen = obj.getNextGenerationBoard(nextGen);
+					}
+					System.out.println("");
+				}
 		}
 	}
 }
